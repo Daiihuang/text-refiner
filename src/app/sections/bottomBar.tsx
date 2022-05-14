f504a1780e9d12b31fbadc149797898fd75f3180
Daiihuang <114500324+Daiihuang@users.noreply.github.com>
2022-05-14 09:40:58 +0000

    Improve title

--- src/app/sections/bottomBar.tsx
+++ src/app/sections/bottomBar.tsx
@@ -12,4 +12,16 @@ const BottomBar = () => {
 
   return (
     <div className="fixed bottom-0 left-0 flex h-12 w-full items-center justify-between rounded-b-extra bg-gray-200 pl-3 pr-3">
-      <div className="flex space-x-3 w-full ite
+      <div className="flex space-x-3 w-full items-center align-middle justify-between">
+        <button onClick={() => setOpenSettings(true)} className="text-gray-500">
+          <CiSettings size={24} className="font-bold" />
+        </button>
+        <Select
+          size="sm"
+          className="w-4/12 text-xs"
+          placeholder={settingContext.model || "Model"}
+          value={settingContext.model || ""}
+          onChange={(event) => settingContext.setModel(event.target.value)}
+        >
+          {settingContext.provider?.models?.map((model) => (
+            <SelectItem value={mod
