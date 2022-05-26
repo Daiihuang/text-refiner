431e1da032c5480278182e5fd9927670b702e0a1
Daiihuang <114500324+Daiihuang@users.noreply.github.com>
2022-05-26 14:56:04 +0000

    Update site/zh-cn/lite/g3doc/guide/ops_version.md

--- src/app/types/translate.ts
+++ src/app/types/translate.ts
@@ -14,4 +14,18 @@ interface ResultTexts {
   refine?: string;
 }
 
-type Mode = "Trans
+type Mode = "Translate" | "Correct" | "Refine";
+
+interface TranslateContextType {
+  languageConfig: LanguageConfig;
+  inputText?: string;
+  result?: ResultTexts;
+  translating?: boolean;
+  currentMode: Mode;
+
+  changeLangConfig: (lang: LanguageConfig) => void;
+  changeInputText: (text: string) => void;
+  changeResult: (result: ResultTexts) => void;
+  setTranslating: (isTranslating: boolean) => void;
+  setCurrentMode: (mode: Mode) => void;
+}
