9573b27b43e3f0aecb62d58af6468e1611573379
Daiihuang <114500324+Daiihuang@users.noreply.github.com>
2024-01-28 10:33:05 +0000

    merge origin

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -8,4 +8,16 @@ const DEFAULT_TRANSLATION_PROMPT: &str = "
     Example 1:
         Original langue: English, Target language: Tiếng Việt
         Input: I'm running
-        Your answer: <ans>Tôi đang 
+        Your answer: <ans>Tôi đang chạy bộ</ans>
+    Example 2:
+        Original langue: English, Target language: Español
+        Input: I'm running
+        Your answer: <ans>Estoy corriendo</ans>
+    Please translate the text in original language {original_lang} in the the text block below to {target_lang}:
+";
+const DEFAULT_CORRECTION_PROMPT: &str = "
+    You're a teacher and you're correcting a student's homework.
+    Only answer the correct text without explanation,
+    put your answer in <ans></ans> block and do not include anything else because only the text inside <ans></ans> block will be extracted.
+    Example:
+        I
